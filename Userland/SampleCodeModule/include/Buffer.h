da850c40ff869fadac6c41f781154ad71acf7cc5
jrambau <120398013+jrambau@users.noreply.github.com>
2024-05-22 16:55:34 -0300

    Add files via upload

---
+++ Userland/SampleCodeModule/include/Buffer.h
@@ -0,0 +1,8 @@
+#ifndef __BUFFER_H__
+#define __BUFFER_H__
+
+#define BUFFER_SIZE 128
+
+void clearBuffer();
+
+#endif
---
+++ Userland/SampleCodeModule/include/Shell.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+int shellInit();
+
+
+#endif //SHELL_H
