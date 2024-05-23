9f85ec23eb52a70b8f59976d6c96380f88c46aa5
ivovila <ivilamowski@itba.edu.ar>
2024-05-23 14:16:21 -0300

    move files

--- Userland/SampleCodeModule/Shell.c
+++ Userland/SampleCodeModule/Shell.c
@@ -1,6 +1,7 @@
 #include "include/Shell.h"
 #include "include/Buffer.h"
 #include "include/lib.h"
+#include "include/usrSysCall.h"
 #include <stdio.h>
 static char buffer[BUFFER_SIZE] = {0};
 
@@ -45,6 +46,58 @@ void bufferControl(){
     return;
 }
 
+// Define an array to store command strings
+const char *commands[] = {
+    "eliminator",
+    "time",
+    "check Exception",
+    "incSize",
+    "getRegisters",
+    "clear"
+};
+
+void lineRead(char *buffer) {
+    if (strcmp(buffer, "help") == 0) {
+        putString("Las siguientes comandos pueden ser utilizadas: \n");
+        
+        // Loop through the commands array and print each command
+        for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++) {
+            putString(commands[i]);
+            putString("\n");
+        }
+    }
+    if (strcmp(buffer, "eliminator") == 0) {
+        // Do something for the "eliminator" command
+        putString("playing eliminator");
+        putString("\n");
+
+    }
+    if (strcmp(buffer, "time") == 0) {
+        char * time;
+        call_timeClock(time);
+        putString(time);
+        putString("\n");
+    }
+    if (strcmp(buffer, "check Exception") == 0) {
+        // Do something for the "check" command
+        putString("checking exception");
+        putString("\n");
+    }
+    if (strcmp(buffer, "incSize") == 0) {
+        // Do something for the "incSize" command
+        putString("incrementing size");
+        putString("\n");
+    }
+    if (strcmp(buffer, "getRegisters") == 0) {
+        // Do something for the "getRegisters" command
+        putString("geting Registers");
+        putString("\n");
+    }
+    if(strcmp(buffer,"clean")){
+        //clearn a la terminal y empezarla de cero
+        
+    }
+}
 
 int shellInit() {
     while(1){
