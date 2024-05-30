ef3fd1cdff8067795e6f54f36167b11959565378
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-05-30 14:52:24 -0300

    fixed setfont

--- Userland/SampleCodeModule/lib.c
+++ Userland/SampleCodeModule/lib.c
@@ -88,16 +88,16 @@ int strcmp( char *str1,  char *str2) {
     }
     return *(unsigned char *)str1 - *(unsigned char *)str2;
 }
-int strcmpSpace(char *str1, char *str2 ){
-    while (*str1 && *str2 && *str1 == *str2 ) {
-        if(*str1 == ' ' && *str2 == ' '){
-            return 0;
+int strcmpSpace(char *str1, char *str2) {
+    while (*str1 && *str2 && *str1 != ' ' && *str2 != ' ' ) {
+      if (*str1 != *str2) {
+            return 1; 
         }
         str1++;
         str2++;
     }
-
-    return *(unsigned char *)str1 - *(unsigned char *)str2;
+    
+    return 0;
 }
 
 int pow(int base, int pow){
