94a40c213f4b27e5aeb79817e296ee9ea957d3e2
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-06-03 22:47:21 -0300

    algunos cambios extra

--- Userland/SampleCodeModule/Shell.c
+++ Userland/SampleCodeModule/Shell.c
@@ -64,7 +64,7 @@ const char *commands[] = {
     "getRegisters: Show the actual state of the registers.",
     "clear:        Empty the terminal.",
     "exit:         Kills the terminal.",
-    "Div0:         Test the exception of the cero division.",
+    "div0:         Test the exception of the cero division.",
     "invalidOp:    Test the exception of an invalid operand."
 };
 
@@ -97,7 +97,7 @@ void lineRead(char *buffer) {
          call_clear();
         clearBuffer(buffer);
         return;
-    }else if(strcmp(buffer,"Div0")==0){
+    }else if(strcmp(buffer,"div0")==0){
         call_div0();
         return;
     }else if(strcmp(buffer,"invalidOp")==0){
