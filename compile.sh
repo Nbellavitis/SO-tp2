c27196f545bf0107f7f1efa3a22cc324423d2bfc
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-09-19 16:13:06 -0300

    first

--- compile.sh
+++
@@ -1,6 +0,0 @@
-docker start TPE
-docker exec -ti TPE make clean -C/root/Toolchain
-docker exec -ti TPE make clean -C/root/
-docker exec -ti TPE make -C/root/Toolchain
-docker exec -ti TPE make -C/root/
-docker stop TPE
